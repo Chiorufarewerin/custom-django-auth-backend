544493c481a51444d79363dd8413ead52c4e6822
Artur Beltsov <artur1998g@gmail.com>
2020-03-29 11:15:41 +0500

    add utils function in init

--- custom_django_auth_backend/__init__.py
+++ custom_django_auth_backend/__init__.py
@@ -2,6 +2,7 @@ from .backend import BasicAuthBackend
 from .dto import UserDTO
 from .exceptions import AuthException, WrongUserPasswordException, UnavailbleException
 from .wrapt import wrapt_authentication_backend
+from .utils import get_from_settings_or_env, get_lazy_object, get_logger
 
 
 __all__ = [
@@ -11,4 +12,7 @@ __all__ = [
     'WrongUserPasswordException',
     'UnavailbleException',
     'wrapt_authentication_backend',
+    'get_from_settings_or_env',
+    'get_lazy_object',
+    'get_logger',
 ]
