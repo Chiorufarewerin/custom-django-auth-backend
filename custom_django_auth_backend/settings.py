93af64987bf9fb99183bdd0e2b24e9d7781d7f5b
Artur Beltsov <artur1998g@gmail.com>
2020-03-29 05:13:12 +0500

    envs without django

--- custom_django_auth_backend/settings.py
+++ custom_django_auth_backend/settings.py
@@ -1,5 +1,5 @@
 from .utils import get_from_settings_or_env as get
 
 
-LOGGER_NAME = get('CUSTOM_DJANGO_AUTH_BACKEND_LOGGER_NAME', 'custom_django_auth_backend')
-DEFAULT_USERNAME_PREFIX = get('CUSTOM_DJANGO_AUTH_BACKEND_DEFAULT_USERNAME_PREFIX', 'custom_')
+LOGGER_NAME = get('CUSTOM_AUTH_BACKEND_LOGGER_NAME', 'custom_django_auth_backend')
+DEFAULT_USERNAME_PREFIX = get('CUSTOM_AUTH_BACKEND_DEFAULT_USERNAME_PREFIX', 'custom_')
